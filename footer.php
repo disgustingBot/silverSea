40ea89fb1867020e23f236a1413080955ae90a1d
tomasmparra <tomas.mparra@lattedev.com>
2020-04-09 18:39:08 +0200

    Summary

--- footer.php
+++ footer.php
@@ -1,11 +1,9 @@
 
 
   <footer class="footer" id="footer">
-
+    <?php include('blueBar.php') ?>
   </footer>
-  <sign class="signature">
-    <p>&#60;&#47;&#62; by <a href="https://lattedev.com/" target="_blank" class="latteLink">Latte</a></p>
-  </sign>
+
   <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -5,6 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+  <!-- GOOGLE FONTS -->
+  <link href="https://fonts.googleapis.com/css2?family=Rambla:wght@400;700&display=swap" rel="stylesheet">
+
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
@@ -15,4 +18,39 @@
 
   <header class="header" id="header">
     <?php include('blueBar.php') ?>
+
+    <!-- NAVIGATION BAR -->
+    <img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Silversea Logo">
+  	<?php
+    	$args = array(
+    	  'theme_location' => 'header',
+    	  'depth' => 0,
+    	  'container'	=> false,
+    	  'fallback_cb' => false,
+    	  'menu_class' => 'navBar',
+    	);
+    	wp_nav_menu($args);
+  	?>
+  </header>
+
+  <header class="headerMob" id="headerMob">
+    <?php include('blueBar.php') ?>
+    <div class="upperHeader">
+      <img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Silversea Logo">
+      <div class="hamburgerMenu">
+        <div class="hamStripe"></div>
+        <div class="hamStripe"></div>
+        <div class="hamStripe"></div>
+      </div>
+    </div>
+    <?php
+      $args = array(
+        'theme_location' => 'navBarMobile',
+        'depth' => 0,
+        'container'	=> false,
+        'fallback_cb' => false,
+        'menu_class' => 'navBarMobile',
+      );
+      wp_nav_menu($args);
+      ?>
   </header>
