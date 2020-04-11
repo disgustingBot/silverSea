2756d486eace226206a68865e5dd19da7bb215ae
tomasmparra <tomas.mparra@lattedev.com>
2020-04-11 16:27:20 +0200

    Front page responsive

--- front-page.php
+++ front-page.php
@@ -1,7 +1,74 @@
 <?php get_header(); ?>
 
 <section class="ATF">
-  <img class="frontPageATFBg" src="<?php echo get_template_directory_uri(); ?>/img/footLogo.png" alt="">
+  <img class="frontPageATFBg rowcol1" src="<?php echo get_template_directory_uri(); ?>/img/ATFExampleImg.jpg" alt="">
+  <div class="features rowcol1">
+        <figure class="feature">
+          <img class="featureIcon" src="<?php echo get_template_directory_uri(); ?>/img/costePreciso.png" alt="feature Icon">
+          <figcaption class="featureTxt">
+            <h3 class="featureTitle brandColorTxt">CALCULO DE COSTE PRECISO</h3>
+            <p class="featureP">eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+          </figcaption>
+        </figure>
+        <figure class="feature">
+          <img class="featureIcon" src="<?php echo get_template_directory_uri(); ?>/img/enviosTracking.png" alt="feature Icon">
+          <figcaption class="featureTxt">
+            <h3 class="featureTitle brandColorTxt">ENVÍOS Y TRACKING LOGíSTICO</h3>
+            <p class="featureP">eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+          </figcaption>
+        </figure>
+        <figure class="feature">
+          <img class="featureIcon" src="<?php echo get_template_directory_uri(); ?>/img/compraRecompra.png" alt="feature Icon">
+          <figcaption class="featureTxt">
+            <h3 class="featureTitle brandColorTxt">COMPRA, RECOMPRA Y ALQUILER</h3>
+            <p class="featureP">eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+          </figcaption>
+        </figure>
+      </div>
 </section>
+
+<section class="FrontPageSec1 sectionPadding">
+  <article class="articleAboutUs">
+    <hgroup class="sectionSummary">
+      <h2 class="summaryTitle">About <span class="brandColorTxt">us</span></h2>
+      <h4 class="summaryTxt brandColorTxt">Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip SUBTITULO</h4>
+      <h4 class="summaryTxt">Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven iam, quis nostrud exer citation ullamco laboris nisi ut perspiciatis unde omnis iste natus error sit voluptate.</h4>
+    </hgroup>
+    <button class="btn"><a href="">LEER MÁS</a></button>
+    <img class="aboutUsMedia" src="<?php echo get_template_directory_uri(); ?>/img/aboutUsImg.jpg" alt="">
+    <img class="playBtn" src="<?php echo get_template_directory_uri(); ?>/img/playBtn.png" alt="">
+  </article>
+</section>
+
+<section class="FrontPageSec2 sectionPadding">
+  <article class="articleFPCounter">
+    <hgroup class="sectionSummary">
+      <h2 class="summaryTitle">Silversea <span class="brandColorTxt">en el mundo</span></h2>
+      <h4 class="summaryTxt brandColorTxt">Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip SUBTITULO</h4>
+      <h4 class="summaryTxt">Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven iam, quis nostrud exer citation ullamco laboris nisi ut perspiciatis unde omnis iste natus error sit voluptate.</h4>
+    </hgroup>
+    <div class="counter">
+      <button class="btn"><a href="">LEER MÁS</a></button>
+      <div class="count">
+        <p class="countNumber">30K+</p>
+        <p class="countTxt">CONTENEDORES VENDIDOS</p>
+      </div>
+      <div class="count">
+        <p class="countNumber">3K+</p>
+        <p class="countTxt">CLIENTES</p>
+      </div>
+      <div class="count">
+        <p class="countNumber">200+</p>
+        <p class="countTxt">CENTROS LOGÍSTICOS</p>
+      </div>
+      <div class="count">
+        <p class="countNumber">30+</p>
+        <p class="countTxt">PAÍSES Y TERRITORIOS</p>
+      </div>
+    </div>
+  </div>
+</article>
+</section>
+
 
 <?php get_footer(); ?>
