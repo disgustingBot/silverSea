856336785da3af87d1a44a9d38363652469cc91c
tomasmparra <tomas.mparra@lattedev.com>
2020-04-12 23:19:48 +0200

    Commit

--- functions.php
+++ functions.php
@@ -20,6 +20,7 @@ function gp_init() {
   // add_theme_support( 'wc-product-gallery-zoom' );
   // add_theme_support( 'wc-product-gallery-lightbox' );
   // add_theme_support( 'wc-product-gallery-slider' );
+    add_theme_support( 'post-thumbnails' );
 }
 add_action('after_setup_theme', 'gp_init');
 
