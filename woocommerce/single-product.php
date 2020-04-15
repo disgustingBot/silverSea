f555fbab433cee04ed730f1758d830cd0dc6a241
Rafael Molinero <rafmoline@gmail.com>
2020-04-15 16:19:39 +0200

    archive try

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -17,11 +17,41 @@
 
   <!-- <?php include('breadCrum'); ?> -->
 
-  <button class="backToGallery" type="button" name="button"><< Volver a la búsqueda</button>
-<section class="singleContainer">
-  <div class="imageGallery">
-    <img class="" src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
-  </div>
+  <button class="btn" type="button" name="button"><< Volver a la búsqueda</button>
+  <section class="singleContainer">
+    <!-- <div class="imageGallery">
+      <img class="" src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
+    </div> -->
+    <div class="imageGallery" id="gallery">
+
+        <?php $attachment_ids = $product->get_gallery_attachment_ids(); ?>
+
+
+          <img class="element rowcol1 lazy" onclick="altClassFromSelector('alt','#gallery')" data-url="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
+          <?php $count=0; foreach( $attachment_ids as $attachment_id ) { ?>
+            <img class="element rowcol1 lazy" onclick="altClassFromSelector('alt','#gallery')" data-url="<?php echo $image_link = wp_get_attachment_url( $attachment_id ); ?>" alt="">
+          <?php $count++; } ?>
+
+
+
+
+
+
+
+
+      <div class="singleProductsgalleryBtnsContainer">
+        <button class="singleProductsGalleryBtns" id="nextButton">
+          <svg class="singleProductArrowSVG" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M18.5455 21.18L9.77992 12L18.5455 2.82L15.8469 0L4.36365 12L15.8469 24L18.5455 21.18Z" fill="currentColor"/>
+          </svg>
+        </button>
+        <button class="singleProductsGalleryBtns" id="prevButton">
+          <svg class="singleProductArrowSVG" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5 21.18L14.2713 12L5 2.82L7.85425 0L20 12L7.85425 24L5 21.18Z" fill="currentColor"/>
+          </svg>
+        </button>
+      </div>
+    </div>
     <figure class="atributo">
 
       <img src="http://localhost/Silversea/wp-content/uploads/2020/04/cargaMax.png" alt="">
@@ -87,8 +117,10 @@
         <?php echo get_post_meta( get_the_id(), 'tara' )[0]; ?>
       </figcaption>
     </figure>
+  </section>
 
-</section>
+  <button class="btn ancho" type="button" name="button">Solicitar Cotización</button>
+
 
 
 <?php } wp_reset_query(); ?>
