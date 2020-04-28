2a509d624403fc5e754b49f20277b9c5742acfba
tomasmparra <tomas.mparra@lattedev.com>
2020-04-28 12:11:26 +0200

    asdf

--- front-page.php
+++ front-page.php
@@ -37,13 +37,13 @@
 <section class="FrontPageSec1 sectionPadding">
   <article class="article2">
     <hgroup class="sectionSummary">
-      <h2 class="summaryTitle">About <span class="brandColorTxt">us</span></h2>
+      <h2 class="summaryTitle">Silversea <span class="brandColorTxt">en el mundo</span></h2>
       <h4 class="summaryTxt brandColorTxt">Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip SUBTITULO</h4>
       <h4 class="summaryTxt">Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven iam, quis nostrud exer citation ullamco laboris nisi ut perspiciatis unde omnis iste natus error sit voluptate.</h4>
+      <h4 class="summaryTxt">Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven iam, quis nostrud exer citation ullamco laboris nisi ut perspiciatis unde omnis iste natus error sit voluptate.</h4>
     </hgroup>
-    <img class="article2Media" src="<?php echo get_template_directory_uri(); ?>/img/aboutUsImg.jpg" alt="">
+    <iframe class="article2Media" src="https://www.google.com/maps/d/embed?mid=17c08JkE4KqI6p3EPcDfsiIMtwDveG7D8" width="640" height="480"></iframe>
   </article>
-  <button class="btn"><a href="">LEER MÁS</a></button>
 </section>
 
 <section class="sectionGrey sectionPadding">
@@ -66,16 +66,23 @@
         <p class="countTxt">PAÍSES Y TERRITORIOS</p>
       </div>
     </div>
+  </article>
+</section>
+
+<section class="FrontPageSec1 sectionPadding">
+  <article class="article2">
     <hgroup class="sectionSummary">
-      <h2 class="summaryTitle">Silversea <span class="brandColorTxt">en el mundo</span></h2>
+      <h2 class="summaryTitle">About <span class="brandColorTxt">us</span></h2>
       <h4 class="summaryTxt brandColorTxt">Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip SUBTITULO</h4>
       <h4 class="summaryTxt">Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven iam, quis nostrud exer citation ullamco laboris nisi ut perspiciatis unde omnis iste natus error sit voluptate.</h4>
     </hgroup>
-    <button class="btn"><a href="">LEER MÁS</a></button>
+    <img class="article2Media" src="<?php echo get_template_directory_uri(); ?>/img/aboutUsImg.jpg" alt="">
   </article>
+  <button class="btn"><a href="">LEER MÁS</a></button>
 </section>
-<section class="sectionPadding depotsContainer">
-  <iframe class="depotsMap article2Media" src="https://www.google.com/maps/d/embed?mid=17c08JkE4KqI6p3EPcDfsiIMtwDveG7D8" width="640" height="480"></iframe>
-</section>
+
+
+
+
 
 <?php get_footer(); ?>
