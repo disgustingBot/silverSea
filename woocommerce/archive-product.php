8c0d37e38e7c221e56e8d12f8bdd68f54ccfd7b0
Rafael Molinero <rafmoline@gmail.com>
2020-05-07 16:05:53 +0200

    upload-file changes

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -16,7 +16,205 @@
 
 
 
-<?php include get_template_directory().'/coprAlqui.php' ?>
+
+
+
+<div class="coprAlqui">
+  <div class="sladRadio">
+    <input class="sladRadioInput" onchange="accordionSelector('#destino')" type="radio" id="euro" name="a10" value="euro" checked>
+    <label class="sladRadioLabel" for="euro">COMPRAR</label>
+    <input class="sladRadioInput" onchange="accordionSelector('#destino')" type="radio" id="dollar" name="a10" value="dollar">
+    <div class="sladRadioSignal"></div>
+    <label class="sladRadioLabel" for="dollar">ALQUILAR</label><br>
+  </div>
+
+    <div class="coprAlquiLocation">
+      <h4 class="coprAlquiLocationTitle">Origen</h4>
+
+
+              <div class="selectBox" tabindex="1" id="selectBoxOrigenCountry">
+                <div class="selectBoxButton">
+                  <p class="selectBoxPlaceholder">Pais</p>
+                  <p class="selectBoxCurrent" id="selectBoxCurrentOrigenCountry"></p>
+                </div>
+                <div class="selectBoxList">
+                  <label for="nulOrigenCountry" class="selectBoxOption">
+                    <input
+                      class="selectBoxInput"
+                      id="nulOrigenCountry"
+                      type="radio"
+                      data-slug="0"
+                      data-parent="city"
+                      name="filter_city"
+                      onclick="selectBoxControler('','#selectBoxOrigenCountry','#selectBoxCurrentOrigenCountry')"
+                      value="0"
+                    >
+                    <!-- <span class="checkmark"></span> -->
+                    <p class="colrOptP"></p>
+                  </label>
+                  <label for="barcelona" class="selectBoxOption">
+                    <input
+                      class="selectBoxInput"
+                      id="barcelona"
+                      data-slug="barcelona"
+                      data-parent="city"
+                      type="radio"
+                      name="filter_city"
+                      onclick="selectBoxControler('Barcelona', '#selectBoxOrigenCountry', '#selectBoxCurrentOrigenCountry')"
+                      value="barcelona"
+                    >
+                    <!-- <span class="checkmark"></span> -->
+                    <p class="colrOptP">España</p>
+                  </label>
+                </div>
+              </div>
+
+
+              <div class="selectBox" tabindex="1" id="selectBoxOrigenCity">
+                <div class="selectBoxButton">
+                  <p class="selectBoxPlaceholder">Ciudad</p>
+                  <p class="selectBoxCurrent" id="selectBoxCurrentOrigenCity"></p>
+                </div>
+                <div class="selectBoxList">
+                  <label for="nulOrigenCity" class="selectBoxOption">
+                    <input
+                      class="selectBoxInput"
+                      id="nulOrigenCity"
+                      type="radio"
+                      data-slug="0"
+                      data-parent="city"
+                      name="filter_city"
+                      onclick="selectBoxControler('','#selectBoxOrigenCity','#selectBoxCurrentOrigenCity')"
+                      value="0"
+                    >
+                    <!-- <span class="checkmark"></span> -->
+                    <p class="colrOptP"></p>
+                  </label>
+                  <label for="barcelona" class="selectBoxOption">
+                    <input
+                      class="selectBoxInput"
+                      id="barcelona"
+                      data-slug="barcelona"
+                      data-parent="city"
+                      type="radio"
+                      name="filter_city"
+                      onclick="selectBoxControler('Barcelona', '#selectBoxOrigenCity', '#selectBoxCurrentOrigenCity')"
+                      value="barcelona"
+                    >
+                    <!-- <span class="checkmark"></span> -->
+                    <p class="colrOptP">Barcelona</p>
+                  </label>
+                </div>
+              </div>
+
+
+
+
+
+
+
+
+
+    </div>
+      <div class="coprAlquiLocation Accordion" id="destino">
+        <h4 class="coprAlquiLocationTitle">Destino</h4>
+
+
+                <div class="selectBox" tabindex="1" id="selectBoxOrigenCountry">
+                  <div class="selectBoxButton">
+                    <p class="selectBoxPlaceholder">Pais</p>
+                    <p class="selectBoxCurrent" id="selectBoxCurrentOrigenCountry"></p>
+                  </div>
+                  <div class="selectBoxList">
+                    <label for="nulOrigenCountry" class="selectBoxOption">
+                      <input
+                        class="selectBoxInput"
+                        id="nulOrigenCountry"
+                        type="radio"
+                        data-slug="0"
+                        data-parent="city"
+                        name="filter_city"
+                        onclick="selectBoxControler('','#selectBoxOrigenCountry','#selectBoxCurrentOrigenCountry')"
+                        value="0"
+                      >
+                      <!-- <span class="checkmark"></span> -->
+                      <p class="colrOptP"></p>
+                    </label>
+                    <label for="barcelona" class="selectBoxOption">
+                      <input
+                        class="selectBoxInput"
+                        id="barcelona"
+                        data-slug="barcelona"
+                        data-parent="city"
+                        type="radio"
+                        name="filter_city"
+                        onclick="selectBoxControler('Barcelona', '#selectBoxOrigenCountry', '#selectBoxCurrentOrigenCountry')"
+                        value="barcelona"
+                      >
+                      <!-- <span class="checkmark"></span> -->
+                      <p class="colrOptP">España</p>
+                    </label>
+                  </div>
+                </div>
+
+
+                <div class="selectBox" tabindex="1" id="selectBoxOrigenCity">
+                  <div class="selectBoxButton">
+                    <p class="selectBoxPlaceholder">Ciudad</p>
+                    <p class="selectBoxCurrent" id="selectBoxCurrentOrigenCity"></p>
+                  </div>
+                  <div class="selectBoxList">
+                    <label for="nulOrigenCity" class="selectBoxOption">
+                      <input
+                        class="selectBoxInput"
+                        id="nulOrigenCity"
+                        type="radio"
+                        data-slug="0"
+                        data-parent="city"
+                        name="filter_city"
+                        onclick="selectBoxControler('','#selectBoxOrigenCity','#selectBoxCurrentOrigenCity')"
+                        value="0"
+                      >
+                      <!-- <span class="checkmark"></span> -->
+                      <p class="colrOptP"></p>
+                    </label>
+                    <label for="barcelona" class="selectBoxOption">
+                      <input
+                        class="selectBoxInput"
+                        id="barcelona"
+                        data-slug="barcelona"
+                        data-parent="city"
+                        type="radio"
+                        name="filter_city"
+                        onclick="selectBoxControler('Barcelona', '#selectBoxOrigenCity', '#selectBoxCurrentOrigenCity')"
+                        value="barcelona"
+                      >
+                      <!-- <span class="checkmark"></span> -->
+                      <p class="colrOptP">Barcelona</p>
+                    </label>
+                  </div>
+                </div>
+
+
+
+
+
+
+
+
+
+      </div>
+</div>
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -207,7 +405,7 @@
               foreach ($categories as $cat) {
                 // var_dump($cat->slug);
                 $parent=get_term_by('id', $cat->parent, 'product_cat', 'ARRAY_A')['slug'];
-                if ($parent=="type") {
+                if ($parent=="condition") {
                   include $svgPath . $cat->slug.'.php';
                 }
               }
