81a506156a71d1bfd410e1f4e83ecc0f00fa7a20
tomasmparra <tomas.mparra@lattedev.com>
2020-05-14 10:53:11 +0200

    asdf

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<section class="ATF frontPageATF">
+<section class="ATF frontPageATF sectionColor5">
   <img class="frontPageATFBg rowcol1" src="<?php echo get_template_directory_uri(); ?>/img/ATFExampleImg.jpg" alt="">
 
 
@@ -14,43 +14,68 @@
 
 
   <div class="features">
-        <figure class="feature">
-          <img class="featureIcon" src="<?php echo get_template_directory_uri(); ?>/img/costePreciso.png" alt="feature Icon">
-          <figcaption class="featureTxt">
-            <h3 class="featureTitle brandColorTxt">CALCULO DE COSTE PRECISO</h3>
-            <p class="featureP">eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-          </figcaption>
-        </figure>
-        <figure class="feature">
-          <img class="featureIcon" src="<?php echo get_template_directory_uri(); ?>/img/enviosTracking.png" alt="feature Icon">
-          <figcaption class="featureTxt">
-            <h3 class="featureTitle brandColorTxt">ENVÍOS Y TRACKING LOGíSTICO</h3>
-            <p class="featureP">eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-          </figcaption>
-        </figure>
-        <figure class="feature">
-          <img class="featureIcon" src="<?php echo get_template_directory_uri(); ?>/img/compraRecompra.png" alt="feature Icon">
-          <figcaption class="featureTxt">
-            <h3 class="featureTitle brandColorTxt">COMPRA, RECOMPRA Y ALQUILER</h3>
-            <p class="featureP">eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-          </figcaption>
-        </figure>
-      </div>
+    <figure class="feature">
+      <img class="featureIcon" src="<?php echo get_template_directory_uri(); ?>/img/costePreciso.png" alt="feature Icon">
+      <figcaption class="featureTxt">
+        <h3 class="featureTitle brandColorTxt">CALCULO DE COSTE PRECISO</h3>
+        <p class="featureP">eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+      </figcaption>
+    </figure>
+    <figure class="feature">
+      <img class="featureIcon" src="<?php echo get_template_directory_uri(); ?>/img/enviosTracking.png" alt="feature Icon">
+      <figcaption class="featureTxt">
+        <h3 class="featureTitle brandColorTxt">ENVÍOS Y TRACKING LOGíSTICO</h3>
+        <p class="featureP">eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+      </figcaption>
+    </figure>
+    <figure class="feature">
+      <img class="featureIcon" src="<?php echo get_template_directory_uri(); ?>/img/compraRecompra.png" alt="feature Icon">
+      <figcaption class="featureTxt">
+        <h3 class="featureTitle brandColorTxt">COMPRA, RECOMPRA Y ALQUILER</h3>
+        <p class="featureP">eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+      </figcaption>
+    </figure>
+  </div>
 </section>
 
-<section class="FrontPageSec1 sectionPadding">
-  <article class="article2">
-    <hgroup class="sectionSummary">
-      <h2 class="summaryTitle">About <span class="brandColorTxt">us</span></h2>
-      <h4 class="summaryTxt brandColorTxt">Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip SUBTITULO</h4>
-      <h4 class="summaryTxt">Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven iam, quis nostrud exer citation ullamco laboris nisi ut perspiciatis unde omnis iste natus error sit voluptate.</h4>
-    </hgroup>
-    <img class="article2Media" src="<?php echo get_template_directory_uri(); ?>/img/aboutUsImg.jpg" alt="">
-  </article>
-  <button class="btn"><a href="">LEER MÁS</a></button>
+<section class="sectionPadding card256" id="queContainerINeed">
+  <?php
+  $args=array(
+    'post_type'=>'container',
+  );
+  $container=new WP_Query($args);
+  while($container->have_posts()){$container->the_post(); ?>
+    <style>
+    #queContainerINeed.card<?php echo get_the_id().' #card'.get_the_id() ; ?>{
+      display: flex;
+    }
+    </style>
+    <article class="article2 containerNeeded" id="card<?php echo get_the_id();?>">
+      <hgroup class="sectionSummary">
+        <h2 class="summaryTitle"><?php the_title(); ?></h2>
+        <h4 class="summaryTxt"><?php the_content(); ?></h4>
+      </hgroup>
+      <img class="article2Media" src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
+    </article>
+  <?php } wp_reset_query(); ?>
+
+  <?php
+  $args=array(
+    'post_type'=>'container',
+    'order' => 'ASC',
+  );
+  $container=new WP_Query($args);
+  ?>
+  <select name="cont_selector" class="btn" id="contSelector"  onchange="if (typeof(this.selectedIndex) != 'undefined') altClassFromSelector(this.value, '#queContainerINeed', 'sectionPadding')">
+    <?php
+    $i=0;
+    while($container->have_posts()){$container->the_post(); ?>
+      <option class="contOption" name="option" <?php if ($i==0){ echo 'checked'; } ?>  value="card<?php echo get_the_id();?>"><?php the_title(); ?></option>
+      <?php $i++; } wp_reset_query(); ?>
+    </select>
 </section>
 
-<section class="sectionGrey sectionPadding">
+<section class="sectionPadding sectionColor5">
   <article class="articleCounter">
     <div class="counter">
       <div class="count countTitle">
@@ -73,7 +98,19 @@
   </article>
 </section>
 
-<section class="FrontPageSec1 sectionPadding">
+<section class="sectionPadding">
+  <article class="article2">
+    <hgroup class="sectionSummary">
+      <h2 class="summaryTitle">About <span class="brandColorTxt">us</span></h2>
+      <h4 class="summaryTxt brandColorTxt">Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip SUBTITULO</h4>
+      <h4 class="summaryTxt">Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven iam, quis nostrud exer citation ullamco laboris nisi ut perspiciatis unde omnis iste natus error sit voluptate.</h4>
+    </hgroup>
+    <img class="article2Media" src="<?php echo get_template_directory_uri(); ?>/img/aboutUsImg.jpg" alt="">
+  </article>
+  <button class="btn"><a href="">LEER MÁS</a></button>
+</section>
+
+<section class="sectionPadding sectionColor5">
   <article class="article2">
     <iframe class="article2Media" src="https://www.google.com/maps/d/embed?mid=17c08JkE4KqI6p3EPcDfsiIMtwDveG7D8" width="640" height="480"></iframe>
     <hgroup class="sectionSummary">
@@ -85,46 +122,4 @@
   </article>
 </section>
 
-
-<section class="sectionPadding card256" id="queContainerINeed">
-
-
-<?php
-$args=array(
-  'post_type'=>'container',
-);
-$container=new WP_Query($args);
-while($container->have_posts()){$container->the_post(); ?>
-  <style>
-  #queContainerINeed.card<?php echo get_the_id().' #card'.get_the_id() ; ?>{
-    display: flex;
-  }
-  </style>
-  <article class="article2 containerNeeded" id="card<?php echo get_the_id();?>">
-    <hgroup class="sectionSummary">
-      <h2 class="summaryTitle"><?php the_title(); ?></h2>
-      <h4 class="summaryTxt"><?php the_content(); ?></h4>
-    </hgroup>
-    <img class="article2Media" src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
-</article>
-<?php } wp_reset_query(); ?>
-
-<?php
-$args=array(
-  'post_type'=>'container',
-  'order' => 'ASC',
-);
-$container=new WP_Query($args);
-?>
-<select name="cont_selector" class="btn" id="contSelector"  onchange="if (typeof(this.selectedIndex) != 'undefined') altClassFromSelector(this.value, '#queContainerINeed', 'sectionPadding')">
-  <?php
-  $i=0;
-  while($container->have_posts()){$container->the_post(); ?>
-  <option class="contOption" name="option" <?php if ($i==0){ echo 'checked'; } ?>  value="card<?php echo get_the_id();?>"><?php the_title(); ?></option>
-<?php $i++; } wp_reset_query(); ?>
-</select>
-
-
-
-</section>
 <?php get_footer(); ?>
--- page-team.php
+++ page-team.php
@@ -1,208 +1,87 @@
 <?php get_header(); ?>
 
-<div class="teamATF">
+<!-- <div class="teamATF">
   <hgroup class="sectionSummary teamATFSummary">
     <h1 class="tamATFTitle">SOMOS <span class="brandColorTxt">SILVERSEA</span></h1>
     <h5 class="summaryTxt">Dado los puntos de contacto con toda la cadena de logística en el 2017 se crea la unidad de Silvercargo. Es bajo la misma compañía que se desarrolla y apoya a toda la gama de clientes que tenemos a nivel mundial, que consideraban importante que podamos darle un soporte más amplio. Teniendo en cuenta nuestra red de agentes en más de 70 países, podemos brindarle soporte local en cada lugar, así atendiéndolos de manera personalizada.Dado los puntos de contacto con toda la cadena de logística en el 2017 se crea la unidad de Silvercargo. </h5>
   </hgroup>
+</div>
+<div class="teamATFSec2">
+  <img class="teamATFImg" src="<?php echo get_post_meta( get_the_id(), 'Imagen_Cabecera' )[0]; ?>" alt="">
+  <div class="sectionSummary">
+    <p class="summaryTxt brandColorTxt">Desde sus comienzos en el año 2016, Silversea nace como una empresa enfocada en el trading y leasing de contenedores marítimos a nivel mundial.</p>
+    <p class="summaryTxt">Dado los puntos de contacto con toda la cadena de logística en el 2017 se crea la unidad de Silvercargo. Es bajo la misma compañía que se desarrolla y apoya a toda la gama de clientes que tenemos a nivel mundial.
+    </p>
+    <button class="btn"><a href="">CONTÁCTANOS</a></button>
   </div>
-  <div class="teamATFSec2">
-    <img class="teamATFImg" src="<?php echo get_post_meta( get_the_id(), 'Imagen_Cabecera' )[0]; ?>" alt="">
-    <div class="sectionSummary">
-      <p class="summaryTxt brandColorTxt">Desde sus comienzos en el año 2016, Silversea nace como una empresa enfocada en el trading y leasing de contenedores marítimos a nivel mundial.</p>
-      <p class="summaryTxt">Dado los puntos de contacto con toda la cadena de logística en el 2017 se crea la unidad de Silvercargo. Es bajo la misma compañía que se desarrolla y apoya a toda la gama de clientes que tenemos a nivel mundial.
-      </p>
-      <button class="btn"><a href="">CONTÁCTANOS</a></button>
-    </div>
-  </div>
+</div> -->
+
+<section class="teamATF">
+  <hgroup class="sectionSummary teamATFSummary sectionColor2">
+    <h1 class="tamATFTitle">SOMOS <span class="brandColorTxt">SILVERSEA</span></h1>
+    <h5 class="summaryTxt">Dado los puntos de contacto con toda la cadena de logística en el 2017 se crea la unidad de Silvercargo. Es bajo la misma compañía que se desarrolla y apoya a toda la gama de clientes que tenemos a nivel mundial, que consideraban importante que podamos darle un soporte más amplio. Teniendo en cuenta nuestra red de agentes en más de 70 países, podemos brindarle soporte local en cada lugar, así atendiéndolos de manera personalizada.Dado los puntos de contacto con toda la cadena de logística en el 2017 se crea la unidad de Silvercargo. </h5>
+  </hgroup>
+</section>
 
 
 
-
-<div class="teamSection">
+<section class="teamSection">
   <h3 class="teamSectionTitle">CONOCE A NUESTRO <span class="brandColorTxt">EQUIPO</span>.</h3>
   <div class="areaSelectorCont">
-    <h3 class="areaSelector"><strong>Áreas</strong></h3>
+    <h3 class="areaSelector"><strong>Áreas de Silversea</strong></h3>
     <div class="areaSelector">
-      <p class="areaSelectorTxt" >Sales</p>
+      <p class="areaSelectorTxt" onclick="altClassFromSelector('sales', '#teamCardsContainer', 'teamCardsContainer')">Sales</p>
     </div>
     <div class="areaSelector">
-      <p class="areaSelectorTxt" >Logistics</p>
+      <p class="areaSelectorTxt" onclick="altClassFromSelector('logistic', '#teamCardsContainer', 'teamCardsContainer')" >Logistics</p>
     </div>
     <div class="areaSelector">
-      <p class="areaSelectorTxt" >Administration</p>
+      <p class="areaSelectorTxt" onclick="altClassFromSelector('administration', '#teamCardsContainer', 'teamCardsContainer')" >Administration</p>
     </div>
     <div class="areaSelector">
-      <p class="areaSelectorTxt" >Management</p>
+      <p class="areaSelectorTxt" onclick="altClassFromSelector('management', '#teamCardsContainer', 'teamCardsContainer')" >Management</p>
     </div>
     <div class="areaSelector">
-      <p class="areaSelectorTxt" >C.E.O.</p>
+      <p class="areaSelectorTxt" onclick="altClassFromSelector('ceo', '#teamCardsContainer', 'teamCardsContainer')" >C.E.O.</p>
     </div>
   </div>
-  <div class="teamCardsContainer">
+  <div id="teamCardsContainer" class="teamCardsContainer">
+
     <?php
     $args=array(
       'post_type'=>'equipo',
-      'tax_query' => array(
-        array(
-          'taxonomy' => 'area',    // taxonomy name
-          'field' => 'slug',      // term_id, slug or name
-          'terms' => 'sales',      // term id, term slug or term name
-        )
-      )
     );
     $equipo=new WP_Query();
     $equipo->query($args);
 
     while($equipo->have_posts()){$equipo->the_post(); ?>
+      <?php
+      $terms = get_the_terms( get_the_id(), 'area' );
+      ?>
 
-
-      <article class="card teamCard">
+      <article class="card teamCard <?php echo $terms[0]->slug; ?>">
         <img class="teamCardImg teamCardImg2 rowcol1" src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
         <img class="teamCardImg teamCardImg1 rowcol1" src="<?php echo get_post_meta($post->ID, 'Imagen_secundaria', true); ?>" alt="">
         <div class="teamCardTxt">
-          <p class="teamCardName"><?php the_title(); ?></p>
-          <p class="teamCardPosition"><?php echo get_post_meta($post->ID, 'Cargo', true); ?></p>
-          <a class="teamCardLinkedin brandColorTxt" href="<?php echo get_post_meta( get_the_id(), 'Linkedin' )[0]; ?>" >LinkedIn</a>
-          <p class="teamCardPhone"><?php echo get_post_meta( get_the_id(), 'Telefono' )[0]; ?></p>
-          <p class="teamCardMail"><?php echo get_post_meta( get_the_id(), 'Correo' )[0]; ?></p>
+          <p class="teamCardName teamCardBlock"><?php the_title(); ?></p>
+          <p class="teamCardPosition teamCardBlock"><?php echo get_post_meta($post->ID, 'Cargo', true); ?></p>
+          <a class="teamCardLinkedin teamCardNone brandColorTxt" href="<?php echo get_post_meta( get_the_id(), 'Linkedin' )[0]; ?>" >LinkedIn</a>
+          <p class="teamCardPhone teamCardNone"><?php echo get_post_meta( get_the_id(), 'Telefono' )[0]; ?></p>
+          <p class="teamCardMail teamCardNone"><?php echo get_post_meta( get_the_id(), 'Correo' )[0]; ?></p>
         </div>
       </article>
 
-
     <?php } wp_reset_query(); ?>
 
-    <?php
-    $args=array(
-      'post_type'=>'equipo',
-      'tax_query' => array(
-        array(
-          'taxonomy' => 'area',    // taxonomy name
-          'field' => 'slug',      // term_id, slug or name
-          'terms' => 'CEO',      // term id, term slug or term name
-        )
-      )
-    );
-    $equipo=new WP_Query();
-    $equipo->query($args);
-
-    while($equipo->have_posts()){$equipo->the_post(); ?>
-
-
-      <article class="card teamCard">
-        <img class="teamCardImg teamCardImg2 rowcol1" src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
-        <img class="teamCardImg teamCardImg1 rowcol1" src="<?php echo get_post_meta($post->ID, 'Imagen_secundaria', true); ?>" alt="">
-        <div class="teamCardTxt">
-          <p class="teamCardName"><?php the_title(); ?></p>
-          <p class="teamCardPosition"><?php echo get_post_meta($post->ID, 'Cargo', true); ?></p>
-          <a class="teamCardLinkedin brandColorTxt" href="<?php echo get_post_meta( get_the_id(), 'Linkedin' )[0]; ?>" >LinkedIn</a>
-          <p class="teamCardPhone"><?php echo get_post_meta( get_the_id(), 'Telefono' )[0]; ?></p>
-          <p class="teamCardMail"><?php echo get_post_meta( get_the_id(), 'Correo' )[0]; ?></p>
-        </div>
-      </article>
-
-
-    <?php } wp_reset_query(); ?>
-
-    <?php
-    $args=array(
-      'post_type'=>'equipo',
-      'tax_query' => array(
-        array(
-          'taxonomy' => 'area',    // taxonomy name
-          'field' => 'slug',      // term_id, slug or name
-          'terms' => 'Logistic',      // term id, term slug or term name
-        )
-      )
-    );
-    $equipo=new WP_Query();
-    $equipo->query($args);
-
-    while($equipo->have_posts()){$equipo->the_post(); ?>
-
-
-      <article class="card teamCard">
-        <img class="teamCardImg teamCardImg2 rowcol1" src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
-        <img class="teamCardImg teamCardImg1 rowcol1" src="<?php echo get_post_meta($post->ID, 'Imagen_secundaria', true); ?>" alt="">
-        <div class="teamCardTxt">
-          <p class="teamCardName"><?php the_title(); ?></p>
-          <p class="teamCardPosition"><?php echo get_post_meta($post->ID, 'Cargo', true); ?></p>
-          <a class="teamCardLinkedin brandColorTxt" href="<?php echo get_post_meta( get_the_id(), 'Linkedin' )[0]; ?>" >LinkedIn</a>
-          <p class="teamCardPhone"><?php echo get_post_meta( get_the_id(), 'Telefono' )[0]; ?></p>
-          <p class="teamCardMail"><?php echo get_post_meta( get_the_id(), 'Correo' )[0]; ?></p>
-        </div>
-      </article>
-
-
-    <?php } wp_reset_query(); ?>
-
-    <?php
-    $args=array(
-      'post_type'=>'equipo',
-      'tax_query' => array(
-        array(
-          'taxonomy' => 'area',    // taxonomy name
-          'field' => 'slug',      // term_id, slug or name
-          'terms' => 'Management',      // term id, term slug or term name
-        )
-      )
-    );
-    $equipo=new WP_Query();
-    $equipo->query($args);
-
-    while($equipo->have_posts()){$equipo->the_post(); ?>
-
-
-      <article class="card teamCard">
-        <img class="teamCardImg teamCardImg2 rowcol1" src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
-        <img class="teamCardImg teamCardImg1 rowcol1" src="<?php echo get_post_meta($post->ID, 'Imagen_secundaria', true); ?>" alt="">
-        <div class="teamCardTxt">
-          <p class="teamCardName"><?php the_title(); ?></p>
-          <p class="teamCardPosition"><?php echo get_post_meta($post->ID, 'Cargo', true); ?></p>
-          <a class="teamCardLinkedin brandColorTxt" href="<?php echo get_post_meta( get_the_id(), 'Linkedin' )[0]; ?>" >LinkedIn</a>
-          <p class="teamCardPhone"><?php echo get_post_meta( get_the_id(), 'Telefono' )[0]; ?></p>
-          <p class="teamCardMail"><?php echo get_post_meta( get_the_id(), 'Correo' )[0]; ?></p>
-        </div>
-      </article>
-
-
-    <?php } wp_reset_query(); ?>
-
-    <?php
-    $args=array(
-      'post_type'=>'equipo',
-      'tax_query' => array(
-        array(
-          'taxonomy' => 'area',    // taxonomy name
-          'field' => 'slug',      // term_id, slug or name
-          'terms' => 'Administration',      // term id, term slug or term name
-        )
-      )
-    );
-    $equipo=new WP_Query();
-    $equipo->query($args);
-
-    while($equipo->have_posts()){$equipo->the_post(); ?>
-
-
-      <article class="card teamCard">
-        <img class="teamCardImg teamCardImg2 rowcol1" src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
-        <img class="teamCardImg teamCardImg1 rowcol1" src="<?php echo get_post_meta($post->ID, 'Imagen_secundaria', true); ?>" alt="">
-        <div class="teamCardTxt">
-          <p class="teamCardName"><?php the_title(); ?></p>
-          <p class="teamCardPosition"><?php echo get_post_meta($post->ID, 'Cargo', true); ?></p>
-          <a class="teamCardLinkedin brandColorTxt" href="<?php echo get_post_meta( get_the_id(), 'Linkedin' )[0]; ?>" >LinkedIn</a>
-          <p class="teamCardPhone"><?php echo get_post_meta( get_the_id(), 'Telefono' )[0]; ?></p>
-          <p class="teamCardMail"><?php echo get_post_meta( get_the_id(), 'Correo' )[0]; ?></p>
-        </div>
-      </article>
-
-
-    <?php } wp_reset_query(); ?>
   </div>
-
-
-</div>
-
+</section>
+<section class="teamATFImgCont ">
+  <div class="teamCTA rowcol1">
+    <h3 class="teamCTATitle brandColorTxt">LOREM IPSUM DOLOR SIT.</h3>
+    <p class="teamCTATxt">Suspendisse pulvinar id diam in consequat. Praesent fringilla elementum dapibus. Aenean purus lectus, egestas vel massa ut, gravida aliquam tortor.</p>
+    <button class="btn">Contáctanos</button>
+  </div>
+</section>
+<img class="teamATFImg" src="<?php echo get_post_meta( get_the_id(), 'Imagen_Cabecera' )[0]; ?>" alt="">
 
 <?php get_footer(); ?>
