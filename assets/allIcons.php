abe0f890edd89c3df6e214d1fb81a4ac7ac5c87e
molinerozadkiel <50532987+molinerozadkiel@users.noreply.github.com>
2020-05-21 15:24:34 +0100

    rework on include sistema

---
+++ assets/allIcons.php
@@ -0,0 +1,10 @@
+<div style="display:none">
+  <?php
+  include 'pies6.svg';
+  include 'pies8.svg';
+  include 'pies10.svg';
+  include 'pies20.svg';
+  include 'pies40.svg';
+  include 'pies45.svg';
+  ?>
+</div>
