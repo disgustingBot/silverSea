428ce34f909a98cc6ded4d823f32a34080e0db97
molinerozadkiel <50532987+molinerozadkiel@users.noreply.github.com>
2020-05-25 14:33:29 +0100

    xD

--- header.php
+++ header.php
@@ -16,11 +16,6 @@
 
 
   <template id="selectBoxOptionTemplate">
-    <!-- <label class="label--checkbox ">
-      <input type="checkbox" name="adwords"   class="checkbox my-auto">
-      <p class="selectBoxOptionLabel"></p>
-    </label> -->
-
     <label for="" class="selectBoxOption" data-test="red">
       <input class="selectBoxInput" id="" type="" name="" value="">
       <!-- <span class="checkmark"></span> -->
@@ -82,6 +77,12 @@
     	);
     	wp_nav_menu($args);
   	?>
+
+    <div class="cartButton" onclick="altClassFromSelector('alt', '#cart')">
+      <svg class="cartButtonSvg" aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+        <path fill="currentColor" d="M551.991 64H129.28l-8.329-44.423C118.822 8.226 108.911 0 97.362 0H12C5.373 0 0 5.373 0 12v8c0 6.627 5.373 12 12 12h78.72l69.927 372.946C150.305 416.314 144 431.42 144 448c0 35.346 28.654 64 64 64s64-28.654 64-64a63.681 63.681 0 0 0-8.583-32h145.167a63.681 63.681 0 0 0-8.583 32c0 35.346 28.654 64 64 64 35.346 0 64-28.654 64-64 0-17.993-7.435-34.24-19.388-45.868C506.022 391.891 496.76 384 485.328 384H189.28l-12-64h331.381c11.368 0 21.177-7.976 23.496-19.105l43.331-208C578.592 77.991 567.215 64 551.991 64zM240 448c0 17.645-14.355 32-32 32s-32-14.355-32-32 14.355-32 32-32 32 14.355 32 32zm224 32c-17.645 0-32-14.355-32-32s14.355-32 32-32 32 14.355 32 32-14.355 32-32 32zm38.156-192H171.28l-36-192h406.876l-40 192z"></path>
+      </svg>
+    </div>
   </header>
 
   <header class="headerMob" id="headerMob">
@@ -109,26 +110,20 @@
   ?>
 
 
+
+
   <div class="cart" id="cart">
     <div class="cartHead">
-      <h4 class="cartTitle">soy un carrito</h4>
-      <button class="btn" type="button" onclick="cartController.send()">
+      <h4 class="cartTitle">Tu Carrito</h4>
+      <button class="btn" type="button" onclick="altClassFromSelector('alt', '#finalizarConsulta')">
         Finalizar
       </button>
     </div>
     <div class="cartList"></div>
   </div>
 
-
-  <!-- <div class="cartItem">
-  <p class="cartItemQty">1</p>
-  <?php newSvg('pies20'); ?>
-  <?php newSvg('Dry'); ?>
-  <?php newSvg('DC'); ?>
-  <?php newSvg('NEW'); ?>
-
-  <button class="close" type="button" onclick="cartController.remove('x')">
-  <div class="closeLine"></div>
-  <div class="closeLine"></div>
-</button>
-</div> -->
+  <div class="finalizarConsulta" id="finalizarConsulta">
+    <p>comprAlqui</p>
+    <p>datos de contacto</p>
+    <button class="btn" onclick="cartController.send()">dame precio rapido</button>
+  </div>
