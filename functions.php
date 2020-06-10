753b5cea23b66f0f4488e5efefefc1a6585c96ed
Rafael Molinero <rafmoline@gmail.com>
2020-06-10 21:14:34 +0200

    idioma

--- functions.php
+++ functions.php
@@ -81,11 +81,16 @@ add_action('after_setup_theme', 'gp_init');
 
 function custom_lang_found(){
     $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-    if( $lang == "en" ){
-        $url = get_home_url()."/en/";
-        wp_redirect( $url );
-    }
+    // if( $lang == "en" ){
+        // $url = get_home_url()."/$lang/";
+				$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+				if($url != $actual_link){
+					// wp_redirect( $url );
+					 // var_dump(get_home_url());
+				}
+    // }
 }
+add_action( 'template_redirect', 'custom_lang_found' );
 
 
 
