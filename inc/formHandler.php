3fef9721bc0dd723cb9a7e1fea222c19455a5714
molinerozadkiel <50532987+molinerozadkiel@users.noreply.github.com>
2020-06-22 12:09:20 +0200

    xd

--- inc/formHandler.php
+++ inc/formHandler.php
@@ -53,4 +53,56 @@ function lt_form_handler() {
 
 
 
- ?>
+
+
+
+add_action(        'wp_ajax_lt_ajax_mail', 'lt_ajax_mail');
+add_action( 'wp_ajax_nopriv_lt_ajax_mail', 'lt_ajax_mail');
+function lt_ajax_mail() {
+	$debugMode = true;
+	$respuesta = array();
+
+  require_once 'mailv1.php';
+
+  // $link=$_POST['link'];
+
+	if($_POST['a00'] != ""){
+    $respuesta['gate0'] = 'ROBOT';
+		// $link = add_query_arg( array('status' => 'nope',), $link );
+	} else {
+    $respuesta['gate0'] = 'no es robot';
+
+
+
+    // $email='molinerozadkiel@gmail.com';
+    $email='tomas.moralparra@gmail.com';
+
+		$subject='Test mail';
+    // $message='TESTEEEE';
+    $message=$mail;
+    
+
+    // foreach ($_POST as $key => $value) {
+    //   if ( $key != 'a00' && $key != 'action' && $key != 'link' && $key != 'status' && $key != 'submit' && $key != 'g-recaptcha-response' ) {
+    //     $message=$message.'<strong>'.$key.':</strong> '.$value.' - <br>';
+    //   }
+    // }
+
+    $headers = array('Content-Type: text/html; charset=UTF-8');
+
+
+    if (wp_mail( $email , $subject , $message , $headers )) {
+      // $link = add_query_arg( array( 'status' => 'sent' , ), $link );
+      $respuesta['gate1'] = 'mail enviado';
+    } else {
+      // $link = add_query_arg( array( 'status' => 'error', ), $link );
+      $respuesta['gate1'] = 'error al enviar mensaje';
+    }
+	}
+	if($debugMode){echo wp_json_encode($respuesta);}
+	exit();
+}
+
+
+
+?>
