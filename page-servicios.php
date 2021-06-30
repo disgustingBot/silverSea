87849b88b75fa2dae1166dc7374f04e72268b729
tomasmparra <50798104+tomasmparra@users.noreply.github.com>
2021-06-30 09:29:45 +0200

    Summary

--- page-servicios.php
+++ page-servicios.php
@@ -1,4 +1,18 @@
 <?php get_header(); ?>
+<section class="hero">
+  <?php
+
+  $config = array(
+    'id' => get_post_thumbnail_id(get_the_ID()),
+    'class' => 'frontPageATFBg rowcol1',
+    'sizes' => [['768', '100']],
+    'default_size' => '100',
+    'width' => 1200,
+    'height' => 800,
+  );
+  responsive_img($config);
+  ?>
+</section>
 
 
 <?php get_footer(); ?>
